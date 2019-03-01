2de67be134e27c17169119e25e2f09bafae8bb62
Ethan Knowlton <eknowlton@gmail.com>
2019-03-01 10:25:50 -0500

    fixed alphabet

--- substenc.go
+++ substenc.go
@@ -5,21 +5,24 @@ import (
 	"strings"
 )
 
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const offset = 12
+
 // Encrypt f func(plaintext string) string
 func Encrypt(plaintext string) string {
 	var result strings.Builder
-	alphabet := "abcdefghijklmnopqrstuvwxyz"
-	offset := 12
 	for i := 0; i < len(plaintext); i++ {
 		if string(plaintext[i]) == " " {
 			result.WriteString(" ")
 			continue
 		}
+
 		currentPosition := strings.Index(alphabet, string(plaintext[i]))
 		transposition := currentPosition + offset
 		if transposition > len(alphabet) {
 			transposition = transposition - len(alphabet)
 		}
+
 		result.WriteString(string(alphabet[transposition]))
 	}
 	return result.String()
@@ -28,23 +31,26 @@ func Encrypt(plaintext string) string {
 // Decrypt f func(cipertext string) string
 func Decrypt(cipertext string) string {
 	var result strings.Builder
-	alphabet := "abcdefghijklmnopqrstuvwxyz"
-	offset := 12
 	for i := 0; i < len(cipertext); i++ {
 		if string(cipertext[i]) == " " {
 			result.WriteString(" ")
 			continue
 		}
+
 		currentPosition := strings.Index(alphabet, string(cipertext[i]))
 		transposition := currentPosition - offset
 		if transposition < 0 {
 			transposition = transposition + len(alphabet)
 		}
+
 		result.WriteString(string(alphabet[transposition]))
 	}
 	return result.String()
 }
 
 func main() {
-	fmt.Println("Testing")
+	fmt.Println("Encrypt string 'Hello World': ")
+	result := Encrypt("Hello World")
+	fmt.Printf("Decrypt result: '%s': \n", result)
+	fmt.Println(Decrypt(result))
 }
